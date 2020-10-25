e492816ad1faf34ea68c7daea3671c8ffc061ce3
Muhammad Naufal Al Ghifari <naufalalghifari121@gmail.com>
2020-10-25 21:58:34 +0700

    fifth

--- ltd.py
+++ ltd.py
@@ -2,17 +2,32 @@ list1 = ['jurusan', 'praktikum', 'kampus', 'tahun']
 list2 = ['informatika', 'fungsional', 'UMM', '2020']
 
 list = []
+relist = []
 dictionary = {}
-# penerapan penggabungan 2 list pada 1 tuple
-tuple = (list1[0] + list2[0], list1[1] + list2[1], list1[2] + list2[2], list1[3] + list2[3])
 
+# penerapan penggabungan 2 list pada 1 tuple
+tuple = (list1[0].capitalize() + list2[0].capitalize(), list1[1].capitalize() + list2[1].capitalize(),
+            list1[2].capitalize() + list2[2].capitalize(), list1[3].capitalize() + list2[3])
+retuple = (list1[0].upper() + list2[0].upper(), list1[1].upper() + list2[1].upper(),
+            list1[2].upper() + list2[2], list1[3].upper() + list2[3])
 
 # penerapan penggabungan 2 list pada list dan dictionary
 for i in range(4):
     plus = list1[i] + list2[i]
     list.append(plus)
+
+
+# for i in range(4):
+#     if(list2[i] == list2[2]):
+#         list2[i] = list2[i].lower()
+#     plus = list1[i] + list2[i]
+#     relist.append(plus)
+
+for i in range(4):
     dictionary[list1[i]] = list2[i]
 
 print (list)
+print (relist)
 print (tuple)
-print (dictionary) 
+print (retuple)
+print (dictionary)
--- nfom2.py
+++ nfom2.py
@@ -1,15 +1,38 @@
-numbers = ([1, 7], [2, 0], [4, 5])
+numbers = ((1, 7), (2, 0), (4, 5))
+plus = []
+minus = []
+devide = []
+cubic = []
+
+for x in range(len(numbers)):
+    plus.append(list(numbers[x]))
+    minus.append(list(numbers[x]))
+    devide.append(list(numbers[x]))
+    cubic.append(list(numbers[x]))
+    for y in range(2):
+        plus[x][y]+=2
+        minus[x][y]-=2
+        devide[x][y]/=2
+        cubic[x][y]=pow(cubic[x][y],3)
+
+print (numbers)
+print (plus)
+print (minus)
+print (devide)
+print (cubic)
+
+# numbers = ([1, 7], [2, 0], [4, 5])
 
 # for x in range(len(numbers)):
 #     for y in range(2):
 #         numbers[x][y] = numbers[x][y] * numbers[x][y]
 
-for x in range(len(numbers)):
-    for y in range(2):
-        numbers[x][y] *= 2
+# for x in range(len(numbers)):
+#     for y in range(2):
+#         numbers[x][y] *= 2
 
-for x in range(len(numbers)):
-    for y in range(2):
-        numbers[x][y] += 2
+# for x in range(len(numbers)):
+#     for y in range(2):
+#         numbers[x][y] += 2
 
-print (numbers)
+# print (numbers)
